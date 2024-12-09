8a1552d74b6226335c51747f2bd0e202fd4ed300
en3m7 <144040306+en3m7@users.noreply.github.com>
2024-12-09 04:36:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 document.getElementById('callback-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Предотвращаем отправку формы
+    event.preventDefault();
 
     const name = document.getElementById('callback-name').value;
     const phone = document.getElementById('callback-phone').value;
 
-    // Здесь можно добавить код для отправки данных на сервер
-
     alert(`Спасибо, ${name}! Мы свяжемся с вами по номеру ${phone}.`);
     
-    // Очистка формы
     document.getElementById('callback-form').reset();
 });
